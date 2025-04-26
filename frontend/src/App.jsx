f85d85bd41d130a52f82805f8caa22c8db25ceda
sheepjune <sheepjune@hanmail.net>
2025-04-27 00:38:50 +0900

    toast Axios hookform

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,10 +5,18 @@ import LandingPage from "./pages/LandingPage";
 import RegisterPage from "./pages/RegisterPage";
 import Navbar from "./layout/Navbar";
 import Footer from "./layout/Footer";
+import {ToastContainer} from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 function Layout() {
     return (
         <div className="flex flex-col h-screen justify-between">
+            <ToastContainer
+            position="bottom-right"
+            theme='light'
+            pauseOnHover
+            autoClose={1500}
+            />
             <Navbar />
             <main className="mb-auto w-10/12 max-w-4xl mx-auto">
                 <Outlet />
--- frontend/src/pages/RegisterPage/index.jsx
+++ frontend/src/pages/RegisterPage/index.jsx
@@ -1,13 +1,49 @@
 import React from "react";
+import {useForm} from 'react-hook-form'
+import {useDispatch} from 'react-redux'
+import { registerUser } from "../../store/thunkFunctions";
 
 const RegisterPage = () => {
+const {register, 
+    handleSubmit, 
+    formState: {errors}, 
+    reset} = useForm({ mode:'onChange'})
+    const dispatch = useDispatch();
+
+    const onSubmit = ({email,password,name}) =>{
+        const body={
+            email,
+            password,
+            name,
+            image:'https://via.placeholder.com/600x400?text=no+user+image'
+        }
+        dispatch(registerUser(body));
+        reset();
+    }
+
+    const userEmail={
+        required:"필수 필드 입니다."
+    }
+
+    const userName={
+        required:"필수 필드 입니다."
+    }
+
+    const userPassword={
+        required:"필수 필드입니다.",
+        minLength:{
+            value:6,
+            message:"최소 6자입니다."
+        }
+    }
+
     return (
         <section className="flex flex-col justify-center mt-20 max-w-[400px] m-auto">
             <div className="p-6 bg-white rounded-md shadow-md">
                 <h1 className="text-3xl font-semibold text-center">
                     회원가입
                 </h1>
-                <form className="mt-6">
+                <form className="mt-6" onSubmit={handleSubmit(onSubmit)}>
                     <div className="mb-2">
                         <label
                             htmlFor="email"
@@ -19,7 +55,16 @@ const RegisterPage = () => {
                             type="email"
                             id="email"
                             className="w-full px-4 py-2 mt-2 bg-white border rounded-md"
+                            {...register('email',userEmail)}
                         />
+                        {
+                            errors?.email &&
+                            <div>
+                                <span className="text-red-500">
+                                    {errors.email.message}
+                                </span>
+                            </div>
+                        }
                     </div>
 
                     <div className="mb-2">
@@ -33,7 +78,16 @@ const RegisterPage = () => {
                             type='text'
                             id='name'
                             className="w-full px-4 py-2 mt-2 bg-white border rounded-md"
+                            {...register('name',userName)}
                         />
+                        {
+                            errors?.name &&
+                            <div>
+                                <span className="text-red-500">
+                                    {errors.name.message}
+                                </span>
+                            </div>
+                        }
                     </div>
 
                     <div className="mb-2">
@@ -47,7 +101,16 @@ const RegisterPage = () => {
                             type='password'
                             id='password'
                             className="w-full px-4 py-2 mt-2 bg-white border rounded-md"
+                            {...register('password',userPassword)}
                         />
+                        {
+                            errors?.password &&
+                            <div>
+                                <span className="text-red-500">
+                                    {errors.password.message}
+                                </span>
+                            </div>
+                        }
                     </div>
 
                     <div className="mt-6">
