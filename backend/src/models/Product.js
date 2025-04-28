16b0eab9cb36030750d08584090bb5895200a3f2
sheepjune <jun8547@gmail.com>
2025-04-29 00:29:41 +0900

    Set up basic navigation menu structure

---
+++ backend/src/models/Product.js
@@ -0,0 +1,39 @@
+const { default: mongoose, Schema } = require("mongoose");
+
+const productSchema = mongoose.Schema({
+    writer: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    title: {
+        type: String,
+        maxLength: 30
+    },
+    description:{
+        type: String,
+    },
+    price: {
+        type: Number,
+        default: 0
+    },
+    images: {
+        type: Array,
+        default: []
+    },
+    sold: {
+        type: Number,
+        default: 0
+    },
+    continents: {
+        type: Number,
+        default: 1
+    },
+    views: {
+        type: Number,
+        default: 0
+    }
+},{timestamps: true})
+
+const Product = mongoose.model("Product", productSchema);
+
+module.exports = Product;
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,6 +13,11 @@ import { useEffect } from "react";
 import ProtectedPage from "./pages/ProtectedPage";
 import ProtectedRoutes from "./components/ProtectedRoutes";
 import NotAuthRoutes from "./components/NotAuthRoutes";
+import UploadProductPage from './pages/UploadProductPage/index';
+import CartPage from './pages/CartPage/index';
+import HistoryPage from './pages/HistoryPage/index';
+import DetailProductPage from './pages/DetailProductPage/index';
+
 
 
 function Layout() {
@@ -53,6 +58,10 @@ function App() {
                 {/* 로그인한 사람만  */}
                 <Route element={<ProtectedRoutes isAuth={isAuth}/>}>
                     <Route path="/protected" element={<ProtectedPage />} />
+                    <Route path="/product/upload" element={<UploadProductPage />} />
+                    <Route path="/product/:productId" element={<DetailProductPage />} />
+                    <Route path="/user/cart" element={<CartPage />} />
+                    <Route path="/history" element={<HistoryPage />} />
                 </Route>
 
                 {/* 로그인한 사람은 갈 수 없음 */}
