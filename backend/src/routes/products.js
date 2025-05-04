a372c7caef2aab80d36b4295d08ec57a1f671a49
sheepjune <jun8547@gmail.com>
2025-05-05 01:41:36 +0900

    もっと見る機能を追加

--- backend/src/routes/products.js
+++ backend/src/routes/products.js
@@ -1,46 +1,56 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const auth = require('../middleware/auth');
-const Product = require('../models/Product');
-const multer = require('multer');
+const auth = require("../middleware/auth");
+const Product = require("../models/Product");
+const multer = require("multer");
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, 'uploads/')
+        cb(null, "uploads/");
     },
     filename: function (req, file, cb) {
-        cb(null, `${Date.now()}_${file.originalname}`)
-    }
-})
+        cb(null, `${Date.now()}_${file.originalname}`);
+    },
+});
 
-const upload = multer({ storage: storage }).single('file')
+const upload = multer({ storage: storage }).single("file");
 
-router.post('/image', auth, async (req, res, next) => {
-
-    upload(req, res, err => {
+router.post("/image", auth, async (req, res, next) => {
+    upload(req, res, (err) => {
         if (err) {
             return req.status(500).send(err);
         }
-        return res.json({ fileName: res.req.file.filename })
-    })
+        return res.json({ fileName: res.req.file.filename });
+    });
+});
 
-})
+router.get("/", async (req, res, next) => {
+    const order = req.query.order ? req.query.order : "desc";
+    const sortBy = req.query.sortBy ? req.query.sortBy : "_id";
+    const limit = req.query.limit ? Number(req.query.limit) : 20;
+    const skip = req.query.skip ? Number(req.query.skip) : 0;
+    const term = req.query.searchTerm;
 
-router.get('/', async (req, res, next) => {
     try {
         const products = await Product.find()
-            .populate('writer')            
+            .populate("writer")
+            .sort([[sortBy, order]])
+            .skip(skip)
+            .limit(limit)
+
+        const productsTotal = await Product.countDocuments();
+        const hasMore = skip + limit < productsTotal ? true : false;
 
         return res.status(200).json({
-            products
-        })
-
+            products,
+            hasMore,
+        });
     } catch (error) {
         next(error);
     }
-})
+});
 
-router.post('/', auth, async (req, res, next) => {
+router.post("/", auth, async (req, res, next) => {
     try {
         const product = new Product(req.body);
         product.save();
@@ -48,6 +58,6 @@ router.post('/', auth, async (req, res, next) => {
     } catch (error) {
         next(error);
     }
-})
+});
 
 module.exports = router;
