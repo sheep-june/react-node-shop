45b88aa5b31694156bc547ac4d53d5496d4ff062
sheepjune <jun8547@gmail.com>
2025-05-08 00:21:54 +0900

    Product Infoコンポーネント作成、ショッピングカートページデータ、ショッピングカートページ作成

--- frontend/src/store/userSlice.js
+++ frontend/src/store/userSlice.js
@@ -1,5 +1,14 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { authUser, loginUser, logoutUser, registerUser} from "./thunkFunctions";
+import {
+    addToCart,
+    authUser,
+    getCartItems,
+    loginUser,
+    logoutUser,
+    // payProducts,
+    registerUser,
+    // removeCartItem,
+} from "./thunkFunctions";
 import { toast } from "react-toastify";
 
 const initialState = {
@@ -21,62 +30,91 @@ const userSlice = createSlice({
     reducers: {},
     extraReducers: (builder) => {
         builder
-            .addCase(registerUser.pending, (state)=>{
-                state.isLoading=true;
+            .addCase(registerUser.pending, (state) => {
+                state.isLoading = true;
             })
-            .addCase(registerUser.fulfilled, (state)=>{
-                state.isLoading=false;
-                toast.info('회원가입을 성공했습니다.');
+            .addCase(registerUser.fulfilled, (state) => {
+                state.isLoading = false;
+                toast.info("회원가입을 성공했습니다.");
             })
-            .addCase(registerUser.rejected, (state, action)=>{
-                state.isLoading=false;
-                state.error=action.payload;
+            .addCase(registerUser.rejected, (state, action) => {
+                state.isLoading = false;
+                state.error = action.payload;
                 toast.error(action.payload);
             })
-            .addCase(loginUser.pending, (state)=>{
-                state.isLoading=true;
+
+            .addCase(loginUser.pending, (state) => {
+                state.isLoading = true;
             })
-            .addCase(loginUser.fulfilled, (state, action)=>{
-                state.isLoading=false;
-//                state.userData = action.payload;
-                state.userData = action.payload.user;
+            .addCase(loginUser.fulfilled, (state, action) => {
+                state.isLoading = false;
+                state.userData = action.payload;
                 state.isAuth = true;
-                localStorage.setItem('accessToken',action.payload.accessToken);
+                localStorage.setItem("accessToken", action.payload.accessToken);
             })
-            .addCase(loginUser.rejected, (state, action)=>{
-                state.isLoading=false;
-                state.error=action.payload;
+            .addCase(loginUser.rejected, (state, action) => {
+                state.isLoading = false;
+                state.error = action.payload;
                 toast.error(action.payload);
             })
-            .addCase(authUser.pending, (state)=>{
-                state.isLoading=true;
+
+            .addCase(authUser.pending, (state) => {
+                state.isLoading = true;
             })
-            .addCase(authUser.fulfilled, (state, action)=>{
-                state.isLoading=false;
+            .addCase(authUser.fulfilled, (state, action) => {
+                state.isLoading = false;
                 state.userData = action.payload;
                 state.isAuth = true;
             })
-            .addCase(authUser.rejected, (state, action)=>{
-                state.isLoading=false;
-                state.error=action.payload;
-                state.isAuth = false;
-                localStorage.removeItem('accessToken');
-            })
-            .addCase(logoutUser.pending, (state)=>{
-                state.isLoading=true;
-            })
-            .addCase(logoutUser.fulfilled, (state)=>{
-                state.isLoading=false;
+            .addCase(authUser.rejected, (state, action) => {
+                state.isLoading = false;
+                state.error = action.payload;
                 state.userData = initialState.userData;
                 state.isAuth = false;
-                localStorage.removeItem('accessToken');
+                localStorage.removeItem("accessToken");
             })
-            .addCase(logoutUser.rejected, (state, action)=>{
-                state.isLoading=false;
-                state.error=action.payload;
+
+            .addCase(logoutUser.pending, (state) => {
+                state.isLoading = true;
+            })
+            .addCase(logoutUser.fulfilled, (state, action) => {
+                state.isLoading = false;
+                state.userData = initialState.userData;
+                state.isAuth = false;
+                localStorage.removeItem("accessToken");
+            })
+            .addCase(logoutUser.rejected, (state, action) => {
+                state.isLoading = false;
+                state.error = action.payload;
                 toast.error(action.payload);
             })
+
+            .addCase(addToCart.pending, (state) => {
+                state.isLoading = true;
+            })
+            .addCase(addToCart.fulfilled, (state, action) => {
+                state.isLoading = false;
+                state.userData.cart = action.payload;
+                toast.info("장바구니에 추가되었습니다.");
+            })
+            .addCase(addToCart.rejected, (state, action) => {
+                state.isLoading = false;
+                state.error = action.payload;
+                toast.error(action.payload);
+            })
+
+            .addCase(getCartItems.pending, (state) => {
+                state.isLoading = true;
+            })
+            .addCase(getCartItems.fulfilled, (state, action) => {
+                state.isLoading = false;
+                state.cartDetail = action.payload;
+            })
+            .addCase(getCartItems.rejected, (state, action) => {
+                state.isLoading = false;
+                state.error = action.payload;
+                toast.error(action.payload);
+            });
     },
 });
-
 export default userSlice.reducer;
